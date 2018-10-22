ccadfa40f03a7ab8d141d3e1cb0bde53f2c3c074
Vishal Patel <vpatel@alliantcreditunion.com>
2018-10-22 16:50:17 -0500

    xamarin forms template

---
+++ Preapproval/Preapproval/Preapproval/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Preapproval.Models;
+
+namespace Preapproval.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
